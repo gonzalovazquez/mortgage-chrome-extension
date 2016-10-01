d9c3a30596a8b5e8d2e057f41513aefce57bcb81
gonzalovazquez <gonzalo.segura1@gmail.com>
2016-10-01 00:01:56 -0400

    clean project

---
+++ js/content.js
@@ -0,0 +1,24 @@
+$(document).ready(function () {
+    console.log('Content script loaded Jquery');
+
+    var firstHref = $("a[href^='http']").eq(0).attr("href");
+    console.log(firstHref);
+
+    // Listening to message from icon click handler
+    chrome.runtime.onMessage.addListener(
+        function(request, sender, sendResponse) {
+            console.log('We got the message');
+            console.log(request);
+            console.log(sender);
+            console.log(sendResponse);
+
+            if (request.message === "clicked_browser_action" ) {
+                firstHref = $("a[href^='http']").eq(0).attr("href");
+                console.log(firstHref);
+
+                // Send back to background that new that we want to open
+                chrome.runtime.sendMessage({"message": "open_new_tab", "url": firstHref});
+            }
+        }
+    );
+}, true);  
