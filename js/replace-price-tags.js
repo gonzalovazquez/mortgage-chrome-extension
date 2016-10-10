af3c816a8fe958a056cf851944a61d8f1a877a74
gonzalovazquez <gonzalovazquez010@gmail.com>
2016-10-10 19:16:18 -0400

    Cleaned content in feature.js

--- js/replace-price-tags.js
+++ js/replace-price-tags.js
@@ -7,5 +7,9 @@ $(document).ready(function() {
   document.body.innerHTML = priceTags.reduce(function(innerHtml, priceTag) {
     return innerHtml.replace(new RegExp('\\' + priceTag, 'g'), "<span class='td-mortgage'>" + priceTag + "</span>");
   }, document.body.innerHTML);
-  $('span.td-mortgage').click(function() {alert('Show mortage calculator')});
+  $('span.td-mortgage').click(function() {  
+    alert('Show mortage calculator');
+    var priceTag = $(event.target).text();
+    console.log(priceTag)
+  });
 });
