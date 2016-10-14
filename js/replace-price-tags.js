3e0fe5b2ec20590746a913781f06b0bc0fbec0db
gonzalovazquez <gonzalovazquez010@gmail.com>
2016-10-14 16:12:21 -0400

    added Modal and remove unused functions

--- js/replace-price-tags.js
+++ js/replace-price-tags.js
@@ -1,15 +1,12 @@
 $(document).ready(function() {
 
   // Create DOM for calculator
-  document.body.innerHTML = document.body.innerHTML + ('<div id="myModal" class="modal"><div class="modal-content"><h1 id="priceTag"></h1></div></div>')
-
-
-  // When the user clicks anywhere outside of the modal, close it
-  window.onclick = function(event) {
-    if (event.target ==   document.getElementById('myModal')) {
-        $('#myModal').css('display', 'none');
-      }
-  }
+  document.body.innerHTML = document.body.innerHTML + (
+    '<div id="ex1" style="display:none;">' +
+    '<p>Thanks for clicking.  That felt good.  <a href="#" rel="modal:close">Close</a> or press ESC</p>' +
+    '<h1 id="priceTag"></h1>' +
+    '</div>'
+    );
 
   // TODO: Do this everytime document changes
   // TODO: improve filter
@@ -17,18 +14,16 @@ $(document).ready(function() {
                   .filter(function(w) { return w[0] === '$'; })
                   .filter(function(e, i, array) { return array.includes(e); });
   document.body.innerHTML = priceTags.reduce(function(innerHtml, priceTag) {
-    return innerHtml.replace(new RegExp('\\' + priceTag, 'g'), "<span class='td-mortgage'>" + priceTag + "</span>");
+    return innerHtml.replace(new RegExp('\\' + priceTag, 'g'), "<a href='#ex1' rel='modal:open'><span class='td-mortgage'>" + priceTag + "</span></a>");
   }, document.body.innerHTML);
 
+  // Grab price and mouse event and insert into DOM modal
   $('span.td-mortgage').click(function() {
-    var priceTag = $(event.target).text();
-    console.log(priceTag);
-
-    $('#priceTag').text(priceTag);
-    document.getElementById('myModal').style.display = 'block';
+    $('#priceTag').text($(event.target).text());
   });
 
+  // Functions for Modal 
   $('#priceTag').click(function() {
     alert('you clicked me')
-  })
+  });
 });
