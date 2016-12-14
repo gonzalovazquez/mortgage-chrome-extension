ed9c42dbd753f3bdb7665cb6ea7e1e8e1b31ce9d
gonzalovazquez <gonzalovazquez010@gmail.com>
2016-12-14 14:59:17 -0500

    Refator dom into object

--- js/mortgage/content.js
+++ js/mortgage/content.js
@@ -4,194 +4,31 @@
   // Global monthly mortgage amount
   var monthlyPayments = 0;
   var prevSavedValueArray = [];
-  var selectedAmount = 0;
-  var openModal;
+  var initializeMortgageModal;
   var objectToApi = {};
 
   // Listening to message from context menu
   chrome.runtime.onMessage.addListener(
     function(request, sender, sendResponse) {
-          openModal(request.message);
+        if (request.type === 'mortgage') {
+            MORTGAGE.methods.domCreation();
+            MORTGAGE.methods.addEventHandler();
+            initializeMortgageModal(request.message);
+            var values = MORTGAGE.methods.calculateMortgage();
+            MORTGAGE.methods.saveAction(values);
+        } else if (request.type === 'stock') {
+            // Create stock DOM
+        }
       }
   );
 
-
-  // UUID Generator
-  function guid() {
-        function s4() {
-            return Math.floor((1 + Math.random()) * 0x10000)
-            .toString(16)
-            .substring(1);
-        }
-        return s4() + s4() + '-' + s4() + '-' + s4() + '-' +
-            s4() + '-' + s4() + s4() + s4();
- }
-
-  // Checking for saved searches
-  (function () {
-    // TODO: Value gets overwritten, need to create new DOM elements based on number of saved searches
-      chrome.storage.sync.get('value', function (res) {
-        prevSavedValueArray = res.value;
-
-        if (prevSavedValueArray.length > 0) {
-          var previousSearchTxt = '\n';
-          prevSavedValueArray.forEach(function (elem, i) {
-            previousSearchTxt += 'Search History ' + (i + 1) + ': ' + '$' + elem.amount + ' monthly' + '\n';
-          });
-
-          document.querySelector('#td-previous').style.display = 'block';
-          document.querySelector('#td-url').setAttribute('href', res.value.url);
-          document.querySelector('#td-prev-amount').textContent = previousSearchTxt;
-        }
-        else {
-          chrome.storage.sync.set({'value': []}, function () {
-            console.log('prevSSavedValueArray is reset to empty array');
-          });
-        }
-      });
-  })();
-
+  
   // Grab price and mouse event and insert into DOM modal
-  openModal = function(amount){
+  initializeMortgageModal = function(amount){
     console.log('Open!', amount);
     document.querySelector('input#td-balance').value = '$' + amount;
     document.querySelector('div#td-widget-pane').style.display = 'block';
     document.querySelector('div#td-widget-pane').classList.add('td-slide');
 
   };
-    
-  // Expand and collapse modal
-  var toggleButton = document.querySelector('.td-widget-pane-toggle-button');
-  toggleButton.addEventListener('click', toggleModal, false);
-
-  function toggleModal() {
-    console.log('Toggle modal');
-    var leftpane = document.querySelector('#td-widget-pane');
-    
-    if (leftpane.classList[0] === 'td-slide') {
-      leftpane.classList.remove('td-slide');
-    } else {
-      leftpane.classList.add('td-slide');
-    }
-  }
-
-  // Calculate Mortgage
-  var calculate = document.querySelector('#td-calculate');
-  calculate.addEventListener('click', calculateMortgage, false);
-
-  function calculateMortgage() {
-    //TODO: Improve mortgage calculation
-    var principalAmount = document.querySelector('input#td-balance').value.replace(/[,$]/g,""),
-        interestRate = document.querySelector('#td-rate').value / 100 / 12,
-        period = document.querySelector('#td-period').value,
-        years = document.querySelector('#td-term').value,
-        numberOfPayments = years * period;
-      
-    var leftSideofEquation = interestRate * Math.pow((1 + interestRate), numberOfPayments);
-    var rightSideofEquation = Math.pow(1 + interestRate, numberOfPayments) - 1;
-    monthlyPayments = parseInt(principalAmount * (leftSideofEquation / rightSideofEquation));
-
-    // Change value dynamically
-    //TODO: Add comma separation
-    document.querySelector('span.td-amount').innerHTML = '$' + monthlyPayments;
-
-    // Show monthly payments
-    document.querySelector('#td-results').style.display = 'block';
-
-    var costOfBorrowing = (monthlyPayments * numberOfPayments) - principalAmount;
-
-    // Chart
-      var ctx = document.getElementById("myChart");
-      var myChart = new Chart(ctx, {
-            type: 'pie',
-            data: {
-                labels: ["Loan", "Cost of Borrowing"],
-                datasets: [{
-                    label: '$',
-                    data: [principalAmount, costOfBorrowing],
-                    backgroundColor: [
-                        'rgba(255, 99, 132, 0.2)',
-                        'rgba(54, 162, 235, 0.2)'
-                    ],
-                    borderColor: [
-                        'rgba(255,99,132,1)',
-                        'rgba(54, 162, 235, 1)'
-                    ],
-                    borderWidth: 1
-                }]
-            },
-            options: {
-                scales: {
-                    yAxes: [{
-                        ticks: {
-                            beginAtZero:true
-                        }
-                    }]
-                }
-            }
-      });
-
-      // Set object
-    objectToApi = {
-            "entry": {
-                "quote": {
-                "amount": principalAmount,
-                "interestRate": document.querySelector('#td-rate').value,
-                "termInMonths": numberOfPayments
-                },
-                    "context": {
-                        "itemLocation": window.location.href,
-                        "site": window.location.hostname
-                    }
-            },
-            "header": {
-                "requestId": guid()
-            }
-        }
-  }
-
-  // Save Research
-
-  var saveButton = document.querySelector('#td-save');
-  saveButton.addEventListener('click', saveResults, false);
-
-  function saveResults() {
-    //TODO: Create a options page to set number of previous saved amounts required. Till then numberOfSavedAmounts is set to 3
-    var numberOfSavedAmounts = 3;
-    var locationOfHome = window.location.href;
-    var valueObjToSave = {
-        url: locationOfHome,
-        amount: monthlyPayments
-    };
-
-    if (prevSavedValueArray.length < numberOfSavedAmounts) {
-        prevSavedValueArray.push(valueObjToSave);
-    } else {
-        prevSavedValueArray.shift();
-        prevSavedValueArray.push(valueObjToSave);
-    }
-
-    //TODO: Implement save function
-      chrome.storage.sync.set({'value': prevSavedValueArray}, function () {
-        // Notify that we saved.
-        alert('Saved');
-        if (prevSavedValueArray.length > 0) {
-            var previousSearchTxt = '\n';
-            prevSavedValueArray.forEach(function (elem, i) {
-                previousSearchTxt = previousSearchTxt + 'Search History ' + (i + 1) + ': ' + '$' + elem.amount + ' monthly' + '\n';
-            });
-            document.querySelector('#td-prev-amount').innerHTML = previousSearchTxt;
-            //TODO: Call API with saved parameters
-            // Send Message to background.js to POST Object
-            var port = chrome.runtime.connect({name: "mortgage"});
-            port.postMessage(objectToApi);
-            
-        } else {
-            chrome.storage.sync.set({'value': []}, function () {
-              console.log('prevSavedValueArray is reset to empty array');
-            });
-        }
-        console.log('Saved');
-      });
-  }
 })();
--- js/mortgage/create-dom.js
+++ js/mortgage/create-dom.js
@@ -1,137 +1,264 @@
-(function () {
-  // Create DOM for calculator
-  var mainContainer = document.createElement('div');
-  mainContainer.setAttribute('id', 'td-widget-pane');
-  var mainTitle = document.createElement('h1');
-  mainTitle.setAttribute('id', 'td-title');
-  mainTitle.innerHTML = 'Mortgage Calculator';
-  var banner = document.createElement('span');
-  banner.setAttribute('class', 'td-banner');
-  var image = document.createElement('img');
-  image.setAttribute('class', 'td-logo');
-  image.setAttribute('src', 'https://upload.wikimedia.org/wikipedia/commons/a/a4/Toronto-Dominion_Bank_logo.svg');
-  var innerContainer = document.createElement('div');
-  innerContainer.setAttribute('class', 'td-inner-container');
-  var balance = document.createElement('h4');
-  balance.setAttribute('id', 'td-balance');
-  var calculateText = document.createElement('p');
-  calculateText.setAttribute('class', 'td-paragraph');
-  var strongText = document.createElement('strong');
-  strongText.innerHTML = 'What would you like to calculate?';
-  calculateText.appendChild(strongText);
-  var anotherContainer = document.createElement('div');
-  var loanBalance = document.createElement('label');
-  loanBalance.setAttribute('class', 'td-mortgage-label');
-  loanBalance.innerHTML = 'Loan Balance';
-  var inputBalance = document.createElement('input');
-  inputBalance.setAttribute('id', 'td-balance');
-  inputBalance.type = 'text';
-  anotherContainer.appendChild(loanBalance);
-  anotherContainer.appendChild(inputBalance);
-  var interestRateContainer = document.createElement('div');
-  var rateLabel = document.createElement('label');
-  rateLabel.setAttribute('class', 'td-mortgage-label');
-  rateLabel.innerHTML = 'Interest rate (%)';
-  var rateInput = document.createElement('input');
-  rateInput.setAttribute('id', 'td-rate');
-  rateInput.type = 'text';
-  interestRateContainer.appendChild(rateLabel);
-  interestRateContainer.appendChild(rateInput);
-  var loanContainer = document.createElement('div');
-  var loanLabel = document.createElement('label');
-  loanLabel.setAttribute('class', 'td-mortgage-label');
-  loanLabel.innerHTML = 'Loan term (years)';
-  var loanInput = document.createElement('input');
-  loanInput.setAttribute('id', 'td-term');
-  loanInput.type = 'text';
-  loanContainer.appendChild(loanLabel);
-  loanContainer.appendChild(loanInput);
-  var periodContainer = document.createElement('div');
-  var periodLabel = document.createElement('label');
-  periodLabel.setAttribute('class', 'td-mortgage-label');
-  periodLabel.innerHTML = 'Period';
-  var selectList = document.createElement("select");
-  selectList.setAttribute('id', 'td-period');
-  var opt = [{title: 'Monthly', value: '12'}, {title: 'Bimonthly', value: '6'}];
-  for (var i = 0; i < opt.length; i++) {
-    var option = document.createElement("option");
-    option.value = opt[i].value;
-    option.text = opt[i].title;
-    selectList.appendChild(option);
-}
-  periodContainer.appendChild(periodLabel);
-  periodContainer.appendChild(selectList);
+// global namespace
+var MORTGAGE = MORTGAGE || {};
 
-  innerContainer.appendChild(calculateText);
-  innerContainer.appendChild(balance);
-  innerContainer.appendChild(anotherContainer);
-  innerContainer.appendChild(interestRateContainer);
-  innerContainer.appendChild(loanContainer);
-  innerContainer.appendChild(periodContainer);
+bar
 
-  var buttonContainer = document.createElement('div');
-  buttonContainer.setAttribute('id', 'td-button-container');
-  var calculateButton = document.createElement('button');
-  calculateButton.setAttribute('class', 'td-btn td-btn-2 td-btn-2a td-btn-fl');
-  calculateButton.setAttribute('id', 'td-calculate');
-  calculateButton.innerHTML = 'Calculate';
-  var saveButton = document.createElement('button');
-  saveButton.setAttribute('class', 'td-btn td-btn-2 td-btn-2a');
-  saveButton.setAttribute('id', 'td-save');
-  saveButton.innerHTML = 'Save';
-  buttonContainer.appendChild(calculateButton);
-  buttonContainer.appendChild(saveButton);
+// sub namespace
+MORTGAGE.methods = {};
+MORTGAGE.event = {};
 
-  var resultContainer = document.createElement('div');
-  resultContainer.setAttribute('id', 'td-results');
-  var monthlyHeadline = document.createElement('h3');
-  var spanAmount = document.createElement('span');
-  spanAmount.setAttribute('class', 'td-amount');
-  monthlyHeadline.appendChild(spanAmount);
-  resultContainer.appendChild(monthlyHeadline);
-  var innerContainer2 = document.createElement('div');
-  innerContainer2.setAttribute('class', 'td-inner-container');
-  var previousSearch = document.createElement('div');
-  previousSearch.setAttribute('id', 'td-previous');
-  var previousSearchText = document.createElement('p');
-  previousSearchText.setAttribute('class', 'td-paragraph');
-  var previousStrong = document.createElement('strong');
-  previousStrong.innerHTML = 'Previous Searches:';
-  previousSearchText.appendChild(previousStrong);
-  var previousLink = document.createElement('a');
-  previousLink.setAttribute('id', 'td-url');
-  previousLink.setAttribute('href', '#');
-  previousLink.innerHTML = 'Link: ';
-  var spanPrevious = document.createElement('span');
-  spanPrevious.setAttribute('id', 'td-prev-amount');
-  previousSearch.appendChild(previousSearchText);
-  previousSearch.appendChild(previousLink);
-  previousSearch.appendChild(spanPrevious);
+MORTGAGE.methods = {
+  domCreation: function() {
+    // Create DOM for calculator
+    var mainContainer = document.createElement('div');
+    mainContainer.setAttribute('id', 'td-widget-pane');
+    var mainTitle = document.createElement('h1');
+    mainTitle.setAttribute('id', 'td-title');
+    mainTitle.innerHTML = 'Mortgage Calculator';
+    var banner = document.createElement('span');
+    banner.setAttribute('class', 'td-banner');
+    var image = document.createElement('img');
+    image.setAttribute('class', 'td-logo');
+    image.setAttribute('src', 'https://upload.wikimedia.org/wikipedia/commons/a/a4/Toronto-Dominion_Bank_logo.svg');
+    var innerContainer = document.createElement('div');
+    innerContainer.setAttribute('class', 'td-inner-container');
+    var balance = document.createElement('h4');
+    balance.setAttribute('id', 'td-balance');
+    var calculateText = document.createElement('p');
+    calculateText.setAttribute('class', 'td-paragraph');
+    var strongText = document.createElement('strong');
+    strongText.innerHTML = 'What would you like to calculate?';
+    calculateText.appendChild(strongText);
+    var anotherContainer = document.createElement('div');
+    var loanBalance = document.createElement('label');
+    loanBalance.setAttribute('class', 'td-mortgage-label');
+    loanBalance.innerHTML = 'Loan Balance';
+    var inputBalance = document.createElement('input');
+    inputBalance.setAttribute('id', 'td-balance');
+    inputBalance.type = 'text';
+    anotherContainer.appendChild(loanBalance);
+    anotherContainer.appendChild(inputBalance);
+    var interestRateContainer = document.createElement('div');
+    var rateLabel = document.createElement('label');
+    rateLabel.setAttribute('class', 'td-mortgage-label');
+    rateLabel.innerHTML = 'Interest rate (%)';
+    var rateInput = document.createElement('input');
+    rateInput.setAttribute('id', 'td-rate');
+    rateInput.type = 'text';
+    interestRateContainer.appendChild(rateLabel);
+    interestRateContainer.appendChild(rateInput);
+    var loanContainer = document.createElement('div');
+    var loanLabel = document.createElement('label');
+    loanLabel.setAttribute('class', 'td-mortgage-label');
+    loanLabel.innerHTML = 'Loan term (years)';
+    var loanInput = document.createElement('input');
+    loanInput.setAttribute('id', 'td-term');
+    loanInput.type = 'text';
+    loanContainer.appendChild(loanLabel);
+    loanContainer.appendChild(loanInput);
+    var periodContainer = document.createElement('div');
+    var periodLabel = document.createElement('label');
+    periodLabel.setAttribute('class', 'td-mortgage-label');
+    periodLabel.innerHTML = 'Period';
+    var selectList = document.createElement("select");
+    selectList.setAttribute('id', 'td-period');
+    var opt = [{title: 'Monthly', value: '12'}, {title: 'Bimonthly', value: '6'}];
+    for (var i = 0; i < opt.length; i++) {
+      var option = document.createElement("option");
+      option.value = opt[i].value;
+      option.text = opt[i].title;
+      selectList.appendChild(option);
+    }
+    periodContainer.appendChild(periodLabel);
+    periodContainer.appendChild(selectList);
 
-  var collapseContainer = document.createElement('div');
-  collapseContainer.setAttribute('class', 'td-widget-pane-toggle-button-container');
-  var collapseBtn = document.createElement('button');
-  collapseBtn.setAttribute('class', 'td-widget-pane-toggle-button');
-  collapseContainer.appendChild(collapseBtn);
+    innerContainer.appendChild(calculateText);
+    innerContainer.appendChild(balance);
+    innerContainer.appendChild(anotherContainer);
+    innerContainer.appendChild(interestRateContainer);
+    innerContainer.appendChild(loanContainer);
+    innerContainer.appendChild(periodContainer);
 
-  var chartContainer = document.createElement('div');
-  chartContainer.setAttribute('id', 'chart-container');
+    var buttonContainer = document.createElement('div');
+    buttonContainer.setAttribute('id', 'td-button-container');
+    var calculateButton = document.createElement('button');
+    calculateButton.setAttribute('class', 'td-btn td-btn-2 td-btn-2a td-btn-fl');
+    calculateButton.setAttribute('id', 'td-calculate');
+    calculateButton.innerHTML = 'Calculate';
+    var saveButton = document.createElement('button');
+    saveButton.setAttribute('class', 'td-btn td-btn-2 td-btn-2a');
+    saveButton.setAttribute('id', 'td-save');
+    saveButton.innerHTML = 'Save';
+    buttonContainer.appendChild(calculateButton);
+    buttonContainer.appendChild(saveButton);
 
-  // Append childs to main container
-  mainContainer.appendChild(mainTitle);
-  mainContainer.appendChild(innerContainer);
-  mainContainer.appendChild(banner);
-  mainContainer.appendChild(image);
-  mainContainer.appendChild(buttonContainer);
-  mainContainer.appendChild(resultContainer);
-  mainContainer.appendChild(previousSearch);
-  mainContainer.appendChild(innerContainer2);
-  mainContainer.appendChild(collapseContainer);
-  // Last step: append calculator
-  document.body.appendChild(mainContainer);
+    var resultContainer = document.createElement('div');
+    resultContainer.setAttribute('id', 'td-results');
+    var monthlyHeadline = document.createElement('h3');
+    var spanAmount = document.createElement('span');
+    spanAmount.setAttribute('class', 'td-amount');
+    monthlyHeadline.appendChild(spanAmount);
+    resultContainer.appendChild(monthlyHeadline);
+    var innerContainer2 = document.createElement('div');
+    innerContainer2.setAttribute('class', 'td-inner-container');
+    var previousSearch = document.createElement('div');
+    previousSearch.setAttribute('id', 'td-previous');
+    var previousSearchText = document.createElement('p');
+    previousSearchText.setAttribute('class', 'td-paragraph');
+    var previousStrong = document.createElement('strong');
+    previousStrong.innerHTML = 'Previous Searches:';
+    previousSearchText.appendChild(previousStrong);
+    var previousLink = document.createElement('a');
+    previousLink.setAttribute('id', 'td-url');
+    previousLink.setAttribute('href', '#');
+    previousLink.innerHTML = 'Link: ';
+    var spanPrevious = document.createElement('span');
+    spanPrevious.setAttribute('id', 'td-prev-amount');
+    previousSearch.appendChild(previousSearchText);
+    previousSearch.appendChild(previousLink);
+    previousSearch.appendChild(spanPrevious);
 
-  // Chart
-  chartContainer.innerHTML = '<canvas id="myChart" width="400" height="400"></canvas>';
-  innerContainer2.appendChild(chartContainer);
+    var collapseContainer = document.createElement('div');
+    collapseContainer.setAttribute('class', 'td-widget-pane-toggle-button-container');
+    var collapseBtn = document.createElement('button');
+    collapseBtn.setAttribute('class', 'td-widget-pane-toggle-button');
+    collapseContainer.appendChild(collapseBtn);
 
-})();
+    var chartContainer = document.createElement('div');
+    chartContainer.setAttribute('id', 'chart-container');
+
+    // Append childs to main container
+    mainContainer.appendChild(mainTitle);
+    mainContainer.appendChild(innerContainer);
+    mainContainer.appendChild(banner);
+    mainContainer.appendChild(image);
+    mainContainer.appendChild(buttonContainer);
+    mainContainer.appendChild(resultContainer);
+    mainContainer.appendChild(previousSearch);
+    mainContainer.appendChild(innerContainer2);
+    mainContainer.appendChild(collapseContainer);
+    // Last step: append calculator
+    document.body.appendChild(mainContainer);
+
+    // Chart
+    chartContainer.innerHTML = '<canvas id="myChart" width="400" height="400"></canvas>';
+    innerContainer2.appendChild(chartContainer);
+  },
+  addEventHandler: function() {
+    // Expand and collapse modal
+    var toggleButton = document.querySelector('.td-widget-pane-toggle-button');
+    toggleButton.addEventListener('click', toggleModal, false);
+    function toggleModal() {
+      console.log('Toggle modal');
+      var leftpane = document.querySelector('#td-widget-pane');
+      
+      if (leftpane.classList[0] === 'td-slide') {
+        leftpane.classList.remove('td-slide');
+      } else {
+        leftpane.classList.add('td-slide');
+      }
+    }
+  },
+  calculateMortgage: function() {
+    var calculate = document.querySelector('#td-calculate');
+    calculate.addEventListener('click', calculate, false);
+
+    var returnData = function calculateMortgage() {
+      //TODO: Improve mortgage calculation
+      var principalAmount = document.querySelector('input#td-balance').value.replace(/[,$]/g,""),
+          interestRate = document.querySelector('#td-rate').value / 100 / 12,
+          period = document.querySelector('#td-period').value,
+          years = document.querySelector('#td-term').value,
+          numberOfPayments = years * period;
+        
+      var leftSideofEquation = interestRate * Math.pow((1 + interestRate), numberOfPayments);
+      var rightSideofEquation = Math.pow(1 + interestRate, numberOfPayments) - 1;
+      monthlyPayments = parseInt(principalAmount * (leftSideofEquation / rightSideofEquation));
+
+      // Change value dynamically
+      //TODO: Add comma separation
+      document.querySelector('span.td-amount').innerHTML = '$' + monthlyPayments;
+
+      // Show monthly payments
+      document.querySelector('#td-results').style.display = 'block';
+      var costOfBorrowing = (monthlyPayments * numberOfPayments) - principalAmount;
+
+      // Create object with all values
+      var values = {
+        "principalAmount": principalAmount, 
+        "interestRate": interestRate, 
+        "period": period, 
+        "years": years,
+        "numberOfPayments": numberOfPayments,
+        "monthlyPayments": monthlyPayments
+      };
+
+      // Chart
+      var ctx = document.getElementById("myChart");
+      var myChart = new Chart(ctx, {
+            type: 'pie',
+            data: {
+                labels: ["Loan", "Cost of Borrowing"],
+                datasets: [{
+                    label: '$',
+                    data: [principalAmount, costOfBorrowing],
+                    backgroundColor: [
+                        'rgba(255, 99, 132, 0.2)',
+                        'rgba(54, 162, 235, 0.2)'
+                    ],
+                    borderColor: [
+                        'rgba(255,99,132,1)',
+                        'rgba(54, 162, 235, 1)'
+                    ],
+                    borderWidth: 1
+                }]
+            },
+            options: {
+                scales: {
+                    yAxes: [{
+                        ticks: {
+                            beginAtZero:true
+                        }
+                    }]
+                }
+            }
+      });
+
+      return values;
+
+    };
+
+    debugger;
+    return returnData;
+  },
+  saveAction: function(values) {
+    console.log(values);
+    // var saveButton = document.querySelector('#td-save');
+    // saveButton.addEventListener('click', saveResults, false);
+    // // Save Research
+    // function saveResults() {
+    //     objectToApi = {
+    //             "entry": {
+    //                 "quote": {
+    //                 "amount": principalAmount,
+    //                 "interestRate": document.querySelector('#td-rate').value,
+    //                 "termInMonths": numberOfPayments
+    //                 },
+    //                     "context": {
+    //                         "itemLocation": window.location.href,
+    //                         "site": window.location.hostname
+    //                     }
+    //             },
+    //             "header": {
+    //                 "requestId": COMMON.commonMethod.guid()
+    //             }
+    //     }
+    //     //TODO: Call API with saved parameters
+    //     // Send Message to background.js to POST Object
+    //     var port = chrome.runtime.connect({name: "mortgage"});
+    //     port.postMessage(objectToApi);
+
+    //     console.log('Saved');
+    // }
+  }
+};
